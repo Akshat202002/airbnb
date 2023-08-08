cabfd1c9c0d309a0348acc8bd11e227f41f1078a
Akshat202002 <akshatsamdani23@gmail.com>
2023-08-08 19:35:08 +0530

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
+const path = require('path');
+
 module.exports = {
-  "output": {
-    "filename": "[name].pack.js"
+  output: {
+    filename: '[name].pack.js',
+    path: path.resolve(__dirname, 'dist') // Output directory
   },
-  "module": {
-    "rules": [
+  module: {
+    rules: [
       {
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-            "presets": [
-              "babel-preset-env",
-              "babel-preset-react"
-            ]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
-        },
-        "exclude": /node_modules/,
-        "test": /\.js$/
+        }
       }
     ]
   },
-  "entry": {
-    "index": "./index"
+  entry: {
+    index: './index.js' // Change this to your entry file
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Specifies the content base directory
+    },
+    port: 8080, // Change to your desired port
+    open: true // Automatically open in browser
   }
 };
